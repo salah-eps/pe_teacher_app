945642a37fca38afc04f23133e52455dbe0704a2
salah-eps <riadhadam10@gmail.com>
2026-08-31 04:00:52 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() async {
 
   try {
     await Supabase.initialize(
-      url: 'https://hjboexpouhrhnujejeen.supabase.co',
+      url: 'https://xjnoexpouhrhnujejeen.supabase.co',
       anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhqYm9leHBvdWhyaG51andqZW9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxMjA5NDMsImV4cCI6MjEwMzY5Njk0M30.zwLgQo5HGWR7BKcnjtoME3pWkNIcFKvtce3d1oONOpg',
     );
   } catch (e) {
